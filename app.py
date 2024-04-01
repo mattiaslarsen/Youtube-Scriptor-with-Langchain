ac78de044bd0dfd443d137050304783b23140157
MattiasZen <122017245+MattiasZen@users.noreply.github.com>
2024-04-01 22:24:23 +0200

    PromptGetAnswer

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ import os
 from apikey import apikey
 
 import streamlit as st
-from langchain.llms import openai
+from langchain.llms import OpenAI
 
 os.environ['OPENAI_API_KEY'] = apikey
 
-#A App framework
+# App framework
 st.title('✨ Youtube GPT Creator')
 prompt = st.text_input('Plug in your prompt here')
 
-#h
+# Llms
+llm = OpenAI(temperature=0.9)
+
+# Show stuff to the screen if there's a prompt
+if prompt:
+    response = llm(prompt)
+    st.write(response)
+
+
 
