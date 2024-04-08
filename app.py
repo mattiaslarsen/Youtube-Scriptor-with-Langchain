b35facb7ea113cb9dffcc1d17b01037c816f0815
MattiasZen <122017245+MattiasZen@users.noreply.github.com>
2024-04-08 11:13:33 +0200

    update

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from apikey import apikey
 
 import streamlit as st
 from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLM
+
 
 os.environ['OPENAI_API_KEY'] = apikey
 
@@ -11,12 +14,19 @@ os.environ['OPENAI_API_KEY'] = apikey
 st.title('✨ Youtube GPT Creator')
 prompt = st.text_input('Plug in your prompt here')
 
+# Prompt templates
+title_template = PromptTemplate(
+    input_variables= ['topic'],
+    template='Write me a Youtube video title for a video about {topic}'
+)
+
 # Llms
 llm = OpenAI(temperature=0.9)
+title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
 
 # Show stuff to the screen if there's a prompt
 if prompt:
-    response = llm(prompt)
+    response = title_chain.run(topic=prompt)
     st.write(response)
 
 
